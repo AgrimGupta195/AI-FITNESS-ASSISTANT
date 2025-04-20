03903f19a872dd56a1d7cdfbfa46d8c8c1432444
Agrim Gupta <agrimgupt195@gmail.com>
2025-04-20 20:34:21 +0530

    complete

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://touching-asp-57.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/lib/vapi.ts
@@ -0,0 +1,2 @@
+import Vapi from "@vapi-ai/web";
+export const vapi = new Vapi("30f2f4b1-193b-4378-825a-29758fbf5a50");
